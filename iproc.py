c6ce3cf7775f11994c36eae71fc58adeefbbf290
Ved Suthar <vedhsuthar@gmail.com>
2022-05-26 15:53:27 +0530

    Added RAW images
    Added basic raw images which are going to be used for this project initially (might be updated later)
    
    Folder structure
    data/raw/plant_name/disease_name

---
+++ iproc.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 14:11:59 2022
+
+@author: vedhs
+"""
+
+from PIL import Image
+
+'''
+Class to store data and process it
+
+Methods
+    get     //  None    //  None
+'''
+class data:
+    
+    #Numpy arrays to save the data
+    x=None
+    y=None
+    
+    def get(self):
+        Pass
